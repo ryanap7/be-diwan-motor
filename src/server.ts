36bbf44838b433e318ff227420802a6527cbf932
ryanap7 <ryan.aprianto77777@gmail.com>
2025-10-11 03:19:51 +0700

    fixing local error

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
-import 'module-alias/register';
+if (process.env.NODE_ENV === 'production') {
+    require('module-alias/register');
+}
+
 import app from './app';
 import { config } from './config/env';
 import logger from './config/logger';
